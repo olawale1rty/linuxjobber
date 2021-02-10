ebaf44b609e14636c52a128d46a7d5ac48e2067e
Olawale <folarinolawale3415@gmail.com>
2021-02-10 02:10:06 +0100

    lab 15

--- myscrumy/moshoodscrumy/views.py
+++ myscrumy/moshoodscrumy/views.py
@@ -12,8 +12,16 @@ def index(request):
 	return HttpResponse(output)
 
 def move_goal(request, **kwargs):
-	output = ScrumyGoals.objects.get(goal_id=kwargs['goal_id'])
-	return HttpResponse(output) 
+	# output = ScrumyGoals.objects.get(goal_id=kwargs['goal_id'])
+	# return HttpResponse(output)
+
+	## LAB 14
+	try: 
+		obj = ScrumyGoals.objects.get(goal_id=kwargs['goal_id'])
+	except Exception as e: 
+		return render(request, 'moshoodscrumy/exception.html', {'error': 'A record with that goal id does not exist'}) 
+	else: 
+		return HttpResponse(obj.goal_name) 
 
 def add_goal(request):
 	add_goal = ScrumyGoals.objects.create(goal_name = 'Keep Learning Django', goal_id = random.randint(1000, 9999), created_by = 'Louis', moved_by = 'Louis', owner = 'Louis', goal_status = GoalStatus.objects.get(status_name='Weekly Goal'), user = User.objects.get(username='louis'))
@@ -21,13 +29,29 @@ def add_goal(request):
 	return HttpResponse('Saved Successfully')
 
 def home(request):
-	dictionary = {}
+	## LAB 12
 	# goal = ScrumyGoals.objects.all()
 	# output = ', '.join([eachgoal.goal_name for eachgoal in goal])
 	# return HttpResponse(output)
-	goal = ScrumyGoals.objects.get(goal_name='Learn Django')
-	# print(goal[3])
-	dictionary['goal_name'] = goal.goal_name
-	dictionary['goal_id'] = goal.goal_id
-	dictionary['user'] = goal.user 
+
+	## LAB 13
+	# dictionary = {}
+	# goal = ScrumyGoals.objects.get(goal_name='Learn Django')
+	# dictionary['goal_name'] = goal.goal_name
+	# dictionary['goal_id'] = goal.goal_id
+	# dictionary['user'] = goal.user 
+	# return render(request, 'moshoodscrumy/home.html', dictionary)
+
+	## LAB 15
+	dictionary = {}
+	users = User.objects.all()
+	# weekly_goals = GoalStatus.objects.filter(status_name='Weekly Goal')
+	# daily_goals = GoalStatus.objects.filter(status_name='Daily Goal')
+	# verify_goals = GoalStatus.objects.filter(status_name='Verify Goal')
+	# done_goals = GoalStatus.objects.filter(status_name='Done Goal')
+	# dictionary['weekly_goals'] = weekly_goals
+	# dictionary['daily_goals'] = daily_goals
+	# dictionary['verify_goals'] = verify_goals
+	# dictionary['done_goals'] = done_goals
+	dictionary['users'] = users 
 	return render(request, 'moshoodscrumy/home.html', dictionary)
