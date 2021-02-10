7175caa95509dba6161d0785b260e4aa57827ad2
Olawale <folarinolawale3415@gmail.com>
2021-02-10 22:06:42 +0100

    add

--- myscrumy/myscrumy/urls.py
+++ myscrumy/myscrumy/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('moshoodscrumy/', include('moshoodscrumy.urls'), namespace='moshoodscrumy')
+    path('moshoodscrumy/', include('moshoodscrumy.urls'), name='moshoodscrumy')
 ]
